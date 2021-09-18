0313713a5b1fa7089625d39aee434b4ec8fed2a2
BrianLuisOjeda <brianojeda6662@gmail.com>
2021-09-17 23:31:20 -0300

    Le agregue  animaciones a mi proyecto

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
 
 class producto {
-    constructor(id, nombre, categoria, precio, img) {
-        this.id = id;
-        this.nombre = nombre;
-        this.categoria = categoria;
-        this.precio = precio;
-        this.img = img
-    }
+  constructor(id, nombre, categoria, precio, img) {
+    this.id = id;
+    this.nombre = nombre;
+    this.categoria = categoria;
+    this.precio = precio;
+    this.img = img
+  }
 }
 
 let carritoID = $("#carrito");
@@ -23,24 +23,34 @@ let producto1 = new producto(28, "Pizza", "muzzarella", 300, "./img/pizzaMuzzare
 let producto2 = new producto(38, "Pizza", "Jamon-morron", 400, "./img/pizzaJamonMorron.jpg")
 let producto3 = new producto(48, "Pizza", "Napolitana", 350, "./img/pizzaNapolitana.jpg")
 let producto4 = new producto(58, "Pizza", "Fugazzeta", 400, "./img/pizzaFugazzeta.jpg")
+let producto5 = new producto(68, "Pizza", "Cuatro quesos", 450, "./img/pizza-4-quesos.jpg")
+let producto6 = new producto(78, "Pizza", "Pepperoni", 450, "./img/pizzaPepperoni.jpg")
+let producto7 = new producto(88, "Pizza", "Hawaiana", 400, "./img/pizzaHawaiana.jpg")
+let producto8 = new producto(98, "Pizza", "Margarita", 300, "./img/pizzaMargarita.jpg")
+
 
 productos.push(producto1)
 productos.push(producto2)
 productos.push(producto3)
 productos.push(producto4)
+productos.push(producto5)
+productos.push(producto6)
+productos.push(producto7)
+productos.push(producto8)
+
 
 console.log(productos);
 
 for (const producto of productos) {
 
-    imprimir.append(`
+  imprimir.append(`
 <div class="card col-3" data-aos="zoom-in" style="width: 14rem;">
 <img src="${producto.img}" class="card-img-top" alt="...">
 <div class="card-body">
     <h5 class="card-title">${producto.nombre}</h5>
     <p class="card-text">${producto.categoria}</p>
     <p class="card-text"> $ ${producto.precio}</p>
-    <a href="#" class="btn btn-primary" id="añadirCarrito" onClick="añadirCarrito(${producto.id})">Comprar</a>
+    <a href="#" class="btn btn-success" id="añadirCarrito" onClick="añadirCarrito(${producto.id})"><i class="bi bi-cart-plus-fill"></i></a>
 </div>
 </div>
 `)
@@ -50,110 +60,85 @@ for (const producto of productos) {
 
 const añadirCarrito = (idPorOnclick) => {
 
-    const objetoIdentificado = productos.find(e => e.id == idPorOnclick)
-    console.log(objetoIdentificado);
+  const objetoIdentificado = productos.find(e => e.id == idPorOnclick)
+  console.log(objetoIdentificado);
 
 
-    if (JSON.parse(localStorage.getItem("carrito")) != null) {
-        let carritoNEW = JSON.parse(localStorage.getItem("carrito"))
-        carritoNEW.push(objetoIdentificado)
+  if (JSON.parse(localStorage.getItem("carrito")) != null) {
+    let carritoNEW = JSON.parse(localStorage.getItem("carrito"))
+    carritoNEW.push(objetoIdentificado)
 
-        localStorage.setItem("carrito", JSON.stringify(carritoNEW))
-        location.reload()
-    } else {
-        carrito.push(objetoIdentificado)
-        localStorage.setItem("carrito", JSON.stringify(carrito))
-        location.reload()
-    }
+    localStorage.setItem("carrito", JSON.stringify(carritoNEW))
+    location.reload()
+  } else {
+    carrito.push(objetoIdentificado)
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    location.reload()
+  }
 
 }
 
 
 const imprimirCarrito = () => {
 
-    let carritoDelStorage = JSON.parse(localStorage.getItem("carrito"))
+  let carritoDelStorage = JSON.parse(localStorage.getItem("carrito"))
 
-    carritoDelStorage.forEach(e => {
-        $("#compra").append(`
-        <tr>
+  carritoDelStorage.forEach(e => {
+    $("#compra").append(`
+        <tr class="selectorAnimado">
       <th scope="row">1</th>
       <td>${e.nombre} </td>
       <td>${e.categoria}</td>
       <td> $ ${e.precio}</td>
-      <td></td>
-      <td><button type="button" class="btn btn-danger" onclick="borrarProductoCarrito(${e.id})">X</button></td>
+      <td id="Total"> </td>
+      <td><button type="button" id="borrarProducto" class="btn btn-danger btnBorrar"  onclick="borrarProductoCarrito(${e.id})">X</button></td>
     </tr>
 `)
-    })
+  })
 
 }
 imprimirCarrito()
-/*
-const precioTotal = () => {
 
-    let carritoDelStorage = JSON.parse(localStorage.getItem("carrito"))
 
-    let precioTotalCompra = 0;
-
-    carritoDelStorage.forEach(e => {
-
-        precioTotalCarrito = precioTotalCompra + e.precio
-    })
-    if (JSON.parse(localStorage.getItem("carrito"))) {
-        imprimirCarrito()
-    }
-
-    $("#total").textContent = precioTotalCarrito
-    precioTotal()
-}
-*/
 const borrarProductoCarrito = (idPorOnclick) => {
 
-    const carritoDelStorage = JSON.parse(localStorage.getItem("carrito"));
+  
+  const carritoDelStorage = JSON.parse(localStorage.getItem("carrito"));
 
-    const indexIdentificado = carritoDelStorage.findIndex(e => e.id == idPorOnclick)
+  const indexIdentificado = carritoDelStorage.findIndex(e => e.id == idPorOnclick)
 
-    if (indexIdentificado >= 0) {
-        carritoDelStorage.splice(indexIdentificado, 1)
-        localStorage.setItem("carrito", JSON.stringify(carritoDelStorage))
-        location.reload()
-    }
+  if (indexIdentificado >= 0) {
+    carritoDelStorage.splice(indexIdentificado, 1)
+    localStorage.setItem("carrito", JSON.stringify(carritoDelStorage))
+    location.reload()
+  }
+  
 }
+borrarProductoCarrito()
 
 
 $("#modalCarrito").on("click", () => {
 
-    $("#modalCarrito").append(`<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div id="modal-body" class="modal-body">
-        <div class="carrito" id="carrito">
-        <table id="table" class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Categoria</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Total</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody id="compra">
-          
-        </tbody>
-      </table>
-        </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>`)
+  $('html, body').animate({
+    scrollTop: $("#carrito").offset().top
+  }, 2000);
+
 })
+
+const precioTotal = () => {
+
+  let carritoDelStorage = JSON.parse(localStorage.getItem("carrito"))
+
+  let precioTotal = 0;
+
+  carritoDelStorage.forEach(e=> {
+
+      precioTotal = precioTotal + e.precio 
+      console.log(precioTotal);
+  })
+
+  $("#Total").append(`${precioTotal}`)
+  total.textContent =  precioTotal;
+}
+
+precioTotal()
--- js/darkmode.js
+++ js/darkmode.js
@@ -10,12 +10,22 @@ $("#theme").on("click", () => {
 
 const darkTheme = () => {
 
-    $(".nav").css("background-color","black")
+    
+    $(".nav").css("background-color","#1c1c1c")
     $(".nav-link").css("color","white")
-    $(".catalogo").css("background-color","black")
+    $(".catalogo").css("background-color","#141414")
     $(".card").css("background-color","black",)
     $(".card-body").css("color","white")
     $(".montoTotal").css("background-color","black")
+    $("#theme").removeClass("btn-outline-dark")
+    $("#theme").addClass("btn-outline-light")
+    $(".card").css("background-color", "#1c1c1c")
+    $(".carrito").css("background-color", "#1c1c1c")
+    $(".table").css("background-color", "#346751")
+    $("th").css("color", "white")
+    $("td").css("color", "white",
+                "background-color", "#346751"
+    )
 
     localStorage.setItem("theme", "dark")
 
@@ -29,6 +39,14 @@ const lightTheme = () => {
     $(".card").css("background-color","white",)
     $(".card-body").css("color","black")
     $(".montoTotal").css("background-color","white")
+    $("#theme").removeClass("btn-outline-light")
+    $("#theme").addClass("btn-outline-dark")
+    $(".carrito").css("background-color", "white")
+    $(".table").css("background-color", "#346751")
+    $("th").css("color", "white")
+    $("td").css("color", "white",
+                "background-color", "#346751"
+    )
 
     localStorage.setItem("theme", "light")
 
